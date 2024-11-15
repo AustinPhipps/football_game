45c6c087103f1d5bc50590d3ac5e0f72d4d9c3c4
AustinPhipps <162862855+AustinPhipps@users.noreply.github.com>
2024-11-15 13:29:40 -0500

    Add files via upload

---
+++ footballGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Football
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerOneScore = 0;
+            int playerTwoScore = 0;
+            int yardline = 0;
+
+            Random random = new Random();
+
+            while (playerOneScore < 22 && playerTwoScore < 22)
+            {
+
+            }
+        }
+    }
+}
